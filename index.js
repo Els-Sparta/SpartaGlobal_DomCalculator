fca9e977f298884d33235a32b2f96743e8abb341
Els <Emathivannan@spartaglobal.com>
2018-08-09 23:07:20 +0100

    Number buttons work and reset display screen

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+function btn(){
+  var num1 = 0;
+}
 
+var buttons = document.getElementsByClassName("buttonNum");
+var operator = document.getElementsByClassName("operator")
+var display = document.getElementById("screen");
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", change_display);
+}
+
+function change_display(){
+  display.innerHTML = this.innerHTML;
+}
+
+// for (var i = 0; i < operator.length; i++){
+//   buttons[i].addEventListener("click", function(){
+//     display.innerHTML += this.innerHTML;
+//   })
+// }
+
+// var display = document.getElementById("screen");
+//
+// switch(operator){
+//   case "+" :
+//     break;
+//   case "-" :
+//     break;
+//   case "*" :
+//     break;
+//   case "/" :
+//     break;
+//   default:
+// }
